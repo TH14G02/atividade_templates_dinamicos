5e3330179ffcef9c5a7c323c5ec5616f62e724b6
TH14G0 <te770760@gmail.com>
2025-12-05 10:52:49 -0300

    ta terminado

--- app.py
+++ app.py
@@ -9,7 +9,8 @@ CURSOS = [
         "nome": "Informatica",
         "descrição": "Aqui você podera aprender sobre diversas areas da informatica",
         "carga_horaria": "3.450 horas",
-        "imagem": ""
+        "imagem": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQR6uawwzTshrsICaFbcVoJ106hNgKSbm1K3w&s",
+        "descrição_detalhada":"O curso de Informática prepara o aluno para utilizar computadores, softwares e ferramentas tecnológicas de forma eficiente no dia a dia profissional. Os estudantes aprendem desde os fundamentos do sistema operacional até aplicações mais avançadas, como programação básica, manutenção de computadores e uso de ferramentas de produtividade. É ideal para quem deseja se inserir no mercado de trabalho, melhorar suas habilidades digitais ou atuar em áreas que exigem conhecimento tecnológico."
 
     },
     {
@@ -17,14 +18,16 @@ CURSOS = [
         "nome": "Administração",
         "descrição": "Aqui você podera aprender sobre diversos assuntos da area da administração",
         "carga_horaria": "3.200 horas",
-        "imagem": ""
+        "imagem": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRumdwbyXEdO8X7K8OnxS7K8ORWeC9P7L3xfQ&s",
+        "descrição_detalhada":"O curso de Administração desenvolve competências para gerenciar empresas, pessoas, recursos e processos com foco em resultados. Nele, o aluno aprende sobre planejamento, organização, liderança, finanças, marketing, atendimento ao cliente e empreendedorismo. É um curso versátil, indicado para quem deseja atuar em diversos setores do mercado ou abrir o próprio negócio, oferecendo uma visão completa do funcionamento de uma organização."
     },
     {
         "id": 3,
         "nome": "Vestuario",
         "descrição": "Aqui você podera aprender sobre diversos modos de custura na area de vestuario",
         "carga_horaria": "3.200 horas",
-        "imagem":""
+        "imagem":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjk-uKt4lJUAWgLNL8b7vR5wdnpB1fLxzxag&s",
+        "descrição_detalhada": "O curso de Vestuário forma profissionais capacitados para atuar na criação, modelagem e produção de roupas e acessórios. Os alunos aprendem sobre desenho técnico, corte e costura, tendências de moda, reciclagem têxtil, criação de peças e operação de máquinas industriais. É ideal para quem deseja trabalhar com moda, confecção ou design de roupas, desenvolvendo criatividade e habilidades práticas para o setor têxtil."
     }
 ]
 
